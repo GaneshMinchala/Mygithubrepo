9318ef831cc7f04c328e47cc19b4680ff42cdca2
GaneshMinchala <ganeshminchala2001@gmail.com>
2023-09-02 19:19:45 +0530

    Added Maths.class to .gitignore

--- Maths.java
+++ Maths.java
@@ -3,6 +3,7 @@ class Maths
     public static void main(String[] args) 
     {
         System.out.println(add(2,3,5,5));
+        System.out.println(mul(2,3,5,5));
     }
     public static int add (int ...a)
     {
@@ -10,6 +11,15 @@ class Maths
         for(int i : a)
         {
             total = total+i;
+        }
+        return total;
+    }
+     public static int mul (int ...a)
+    {
+        int total=1;
+        for(int i : a)
+        {
+            total = total*i;
         }
         return total;
     }
