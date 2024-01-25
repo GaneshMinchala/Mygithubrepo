bde7f2977f4ef93ff6d703c12ec54aea79f8c388
GaneshMinchala <ganeshminchala2001@gmail.com>
2024-01-25 15:22:02 +0530

    dummy commit due to the '*.class'entered as '*.classS' incorrectly

--- Name.java
+++ Name.java
@@ -3,7 +3,7 @@ class Name
     
         public static void main(String []args)
         {
-            System.out.println("Ganesh Minchala");
+            System.out.println("Ganesh Minchala"+"184e1@o307");
         }
     
 }
